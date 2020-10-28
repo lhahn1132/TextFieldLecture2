8f609a4dfce0a594bb03e811d4eaeb3892f1a67b
Lucy Hahn <lhahn1132@stu.d214.org>
2020-10-28 12:53:41 -0700

    Initial Commit

---
+++ TextFieldLecture2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldLecture2
+//
+//  Created by user171627 on 10/28/20.
+//  Copyright © 2020 user1132. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
